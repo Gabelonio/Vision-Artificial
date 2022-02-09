6ee69fab780198c961a50b5306db80856e9008cb
Gabelonio <gabelonio@hotmail.com>
2022-02-08 21:24:19 -0500

    extrae valores individuales de escala de grises

--- RGBaGris.py
+++ RGBaGris.py
@@ -1,15 +1,22 @@
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 from PIL import Image
+import cv2
+import numpy as np
+np.set_printoptions(threshold=np.inf)
 
-img = Image.open('imagen.jpg')
+img = Image.open('increible.jfif')
 imgGray = img.convert('L')
 imgGray.save('imagen_gris.jpg')
 
-imgGris = Image.open('imagen_gris.jpg')
-imagenRGB = imgGris.convert("RGB")
-valorIndividual = imagenRGB.getpixel((500,150))
-print(valorIndividual)
+arreglo_valores = []
+
+img = cv2.imread("imagen_gris.jpg", 0) 
+for i in range (img.shape[0]): 
+    fila_valores = []
+    for j in range (img.shape[1]): 
+        fila_valores.append(img[i][j])
+    arreglo_valores.append(fila_valores)
 
 """ 
 Imprimir imagen
@@ -17,5 +24,13 @@ imgGris = mpimg.imread('image.png')
 plt.imshow(imgGris, cmap = "gray")
 plt.show()
  """
+
+""" 
+Obtener RGB individual
+imgGris = Image.open('imagen_gris.jpg')
+imagenRGB = imgGris.convert("RGB")
+valorIndividual = imagenRGB.getpixel((500,150))
+print(valorIndividual)
+ancho, alto = imgGris.size  """
 
 
